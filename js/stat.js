4b3869631c668be4e1f27e19d5f803fa31acd6e6
helenmir <helen.mironovaa@mail.ru>
2018-11-19 01:21:49 +0300

    начало решения задачи (без гистограммы)

---
+++ js/stat.js
@@ -0,0 +1,63 @@
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+var CLOUD_X = 100;
+var CLOUD_Y = 10;
+var GAP = 10;
+
+var MESSAGE_X = CLOUD_WIDTH - 40;
+var MESSAGE_Y = 40;
+var MESSAGE_GAP = 20;
+var TEXT_FONT = '16px PT Mono';
+
+//рисуем облако
+var renderCloud = function(ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+}
+
+//пишем текст поздравления с победой
+var renderText = function(ctx) {
+  ctx.font = TEXT_FONT;
+  ctx.fillText('Ура вы победили!', MESSAGE_X, MESSAGE_Y);
+  ctx.fillText('Список результатов:', MESSAGE_X, MESSAGE_Y + MESSAGE_GAP);
+}
+
+// далее нам нужно определить результаты игры
+//определяем максимальное время
+var getMaxElement = function(times) {
+  var maxElement = times[0];
+
+  for (var i = 0; i < times.length; i++) {
+    if (times[i] > maxElement) {
+      maxElement = times[i];
+    }
+  }
+
+  return maxElement;
+};
+
+//задаем функцию смены цвета колонок с результатами игроков
+var changeColor = function(names[i]) {
+  if (names[i] === 'Вы') {
+    return 'rgba(255, 0, 0, 1)';
+  } else {
+    return 'rgba(0, 0, 255, ' + Math.random() + ')';
+  }
+}
+
+//рисуем гистограмму
+var renderGameResults = function(ctx, names, times) {
+  var maxTime = getMaxElement(times);
+  for (var i = 0; i <names.length; i++) {
+
+  }
+}
+
+
+//выводим все это на экран
+window.renderStatistics = function(ctx, names, times) {
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+  renderText(ctx);
+  renderGameResults(ctx, names, times);
+};
